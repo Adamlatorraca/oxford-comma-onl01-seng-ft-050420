7ecda80ece0aa9931887592d3bf6a3b1dd83fd1e
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-06 23:28:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+    array[0...2].join(", ") << ", and #{array[2]}"
+  else array.length > 3
+    array[0...-1].join(", ") << ", and #{array[-1]}"
+  end
 end
+
+# if array length = 1 return it
+# if array length = 2 then add "and" between elements
+# if array length = 3 then add commas and add "and"
+# if array length > 3 then add commas and add "and"
